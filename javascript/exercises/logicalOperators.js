8bb4abc572a1d601d0ac96c44dd133f8993b8624
eizar-paing <eizarpaing.sg@gmail.com>
2024-08-25 12:31:46 +0900

    logical operators

---
+++ javascript/exercises/logicalOperators.js
@@ -0,0 +1,43 @@
+1 = true
+0 = false
+
+
+
+if ( true && true ) { }
+if ( false && true ) {  }
+
+
+mark = 70
+
+if ( mark > 40 && mark < 60) {
+	Grade = C;
+} else if ( mark > 40 && mark < 80) {
+	Grade = B;
+} else if ( mark > 80 && mark < 100) {
+	Grade = B
+}
+
+studentName = "Rose"
+age = 15
+
+if ( studentName == "John" || age == 12) {
+  result = "a"
+} else {
+  result = "b"
+}
+
+
+
+
+// result = "b"
+
+// 1 & 0  = 0
+// 1 & 1 = 1
+// 0 & 1 = 0
+// 0 & 0 = 0
+
+
+// 0 | 0 = 0
+// 0 | 1 = 1
+// 1 | 0 = 1
+// 1 | 1 = 1
